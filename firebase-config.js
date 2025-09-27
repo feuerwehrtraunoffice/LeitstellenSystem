aa4af5b6dfe6e1154c6a7b5cd17486f160daad99
feuerwehrtraunoffice <feuerwehr.traun.office@gmail.com>
2025-09-27 21:38:34 +0200

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// firebase-config.js
+const firebaseConfig = {
+  apiKey: "DEIN_API_KEY",
+  authDomain: "DEIN_PROJECT.firebaseapp.com",
+  projectId: "DEIN_PROJECT",
+  storageBucket: "DEIN_PROJECT.appspot.com",
+  messagingSenderId: "DEINE_SENDER_ID",
+  appId: "DEINE_APP_ID"
+};
+
+firebase.initializeApp(firebaseConfig);
