427ea45c6867c6c0fe903c63a7b78e988863c28a
feuerwehrtraunoffice <feuerwehr.traun.office@gmail.com>
2025-09-27 21:44:00 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,18 @@
 // firebase-config.js
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
 const firebaseConfig = {
-  apiKey: "DEIN_API_KEY",
-  authDomain: "DEIN_PROJECT.firebaseapp.com",
-  projectId: "DEIN_PROJECT",
-  storageBucket: "DEIN_PROJECT.appspot.com",
-  messagingSenderId: "DEINE_SENDER_ID",
-  appId: "DEINE_APP_ID"
+  apiKey: "AIzaSyAQOOB8RtpiV1h4oV17nWXGs8xbje3QxMw",
+  authDomain: "elis-traun.firebaseapp.com",
+  projectId: "elis-traun",
+  storageBucket: "elis-traun.firebasestorage.app",
+  messagingSenderId: "512601294479",
+  appId: "1:512601294479:web:dffe92d2f00ffaeb9852fa",
+  measurementId: "G-GN9QM7R224"
 };
 
-firebase.initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { auth };
